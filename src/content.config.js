3e104b90d53ba7190a6f95725d85932e7d50767f
Lou8is <lis.gpublic@gmail.com>
2025-01-16 15:35:07 +0100

    updates lots of dependencies + cassidoos template

---
+++ src/content.config.js
@@ -0,0 +1,18 @@
+import { defineCollection, z } from "astro:content";
+
+import { glob } from "astro/loaders";
+
+const posts = defineCollection({
+	loader: glob({ pattern: "*.md", base: "./src/posts" }),
+	schema: z.object({
+		title: z.string(),
+		slug: z.string(),
+		description: z.string(),
+        cover: z.string().optional(),
+		added: z.union([z.string(), z.date()]),
+		updated: z.union([z.string(), z.date()]).optional(),
+		tags: z.array(z.string()),
+	}),
+});
+
+export const collections = { posts };
